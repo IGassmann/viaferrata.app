c9062e952c62a5a6eaded5ae96a3bfe85ba20945
Igor Gassmann <igor@igassmann.me>
2024-10-01 16:43:01 +0200

    feat: set global background and font color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${inter.variable} antialiased`}>
+    <html
+      lang="en"
+      className={`${inter.variable} bg-white text-zinc-950 antialiased dark:bg-zinc-900 dark:text-white`}
+    >
       <body>
         {children}
         <Analytics />
