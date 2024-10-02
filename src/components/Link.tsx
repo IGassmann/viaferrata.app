0d7d65a68c71253d5f295faff27f8a727492218e
Igor Gassmann <igor@igassmann.me>
2024-10-02 17:30:44 +0200

    fix: link props type

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,7 +1,9 @@
 import * as Headless from "@headlessui/react";
 import NextLink, { type LinkProps } from "next/link";
 
-export default function Link(props: LinkProps) {
+export default function Link(
+  props: LinkProps & React.ComponentPropsWithRef<"a">,
+) {
   return (
     <Headless.DataInteractive>
       <NextLink {...props} />
