4a40e3ee130e49eaf4b7c83a52e4ee064812a847
Igor Gassmann <igor@igassmann.me>
2024-10-09 11:35:05 +0200

    feat: add routes.difficulty_grade column

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -3,10 +3,20 @@ import { sql } from "drizzle-orm";
 import {
   bigserial,
   interval,
+  pgEnum,
   pgTable,
   timestamp,
   varchar,
 } from "drizzle-orm/pg-core";
+
+export const difficultyGradeEnum = pgEnum("difficulty_grade", [
+  "K1",
+  "K2",
+  "K3",
+  "K4",
+  "K5",
+  "K6",
+]);
 
 export const routes = pgTable("routes", {
   internalID: bigserial("internal_id", { mode: "number" }).primaryKey(),
@@ -16,6 +26,7 @@ export const routes = pgTable("routes", {
     .notNull()
     .$defaultFn(() => PublicID.create(PublicID.PREFIXES.route)),
   name: varchar("name", { length: 256 }).notNull(),
+  difficultyGrade: difficultyGradeEnum().notNull(),
   duration: interval("duration").notNull(),
   createdAt: timestamp("created_at").notNull().defaultNow(),
   updatedAt: timestamp("updated_at")
