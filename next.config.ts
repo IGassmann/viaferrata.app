8bdb3ee6f8f815ab5a5378861c4b32b2993c3928
Igor Gassmann <igor@igassmann.me>
2024-11-08 16:37:08 +0100

    feat: display route images

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,16 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "46s1kywljfszgglh.public.blob.vercel-storage.com",
+        port: "",
+        search: "",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ import db from "@/lib/db";
 import { routes, type DifficultyGrade } from "@/lib/db/schema";
 import { parseDuration } from "@internationalized/date";
 import { CarIcon, TrainIcon } from "lucide-react";
+import Image from "next/image";
 
 async function listRoutes() {
   const routeRows = await db
     .select({
       id: routes.publicID,
       name: routes.name,
+      imageURL: routes.imageURL,
       duration: routes.duration,
       difficultyGrade: routes.difficultyGrade,
       coordinates: routes.coordinates,
@@ -39,16 +41,20 @@ export default async function HomePage() {
   const routes = await listRoutes();
   return (
     <menu className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-6">
-      {routes.map((route) => (
+      {routes.map(async (route) => (
         <li
           className="relative h-full w-full overflow-hidden rounded-lg bg-white shadow-[0px_0px_0px_1px_rgba(9,9,11,0.07),0px_2px_2px_0px_rgba(9,9,11,0.05)] dark:bg-zinc-900 dark:shadow-[0px_0px_0px_1px_rgba(255,255,255,0.1)] dark:before:pointer-events-none dark:before:absolute dark:before:-inset-px dark:before:rounded-xl dark:before:shadow-[0px_2px_8px_0px_rgba(0,_0,_0,_0.20),_0px_1px_0px_0px_rgba(255,_255,_255,_0.06)_inset] forced-colors:outline"
           key={route.id}
         >
-          <img
-            alt=""
-            src={`https://picsum.photos/seed/${encodeURIComponent(route.name)}/512`}
-            className="aspect-[10/7] bg-zinc-300 shadow dark:bg-zinc-800"
-          />
+          <div className="relative aspect-[10/7] bg-zinc-300 shadow dark:bg-zinc-800">
+            <Image
+              alt=""
+              src={route.imageURL}
+              fill
+              sizes="(min-width: 1024px) 25vw, (min-width: 640px) 33.333vw, 50vw"
+              className="object-cover"
+            />
+          </div>
           <div className="space-y-3.5 p-2">
             <div className="text-base/6 font-semibold">{route.name}</div>
 
